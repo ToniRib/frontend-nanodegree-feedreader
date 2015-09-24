b2e0e89c2b847a2c7f4a1fb6d47ad0945f3297b1
ToniRib <tonimarierib@gmail.com>
2015-09-24 10:52:31 -0600

    feat: Menu changes visibility when menu icon is clicked test passes
    The test for ensuring the menu visibility changes when the menu icon
    is clicked is now written and passes. I tested it's failure by commenting
    out the line $('body').toggleClass('menu-hidden'); in app.js.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,13 +61,16 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-        /* TODO: Write a test that ensures the menu changes
+        /* Fifth test - it tests to ensures the menu changes
          * visibility when the menu icon is clicked. This test
-         * should have two expectations: does the menu display when
-         * clicked and does it hide when clicked again.
+         * ensure both that the menu is visible when clicked once
+         * and is not visible when clicked a second time.
          */
         it('changes visibility when the menu icon is clicked', function() {
-
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').trigger('click');
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
 
